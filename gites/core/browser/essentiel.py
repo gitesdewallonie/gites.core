fd97ea5b9f6e8893f0f196d2cef86cfeff722fab
francoisbr <francois@affinitic.be>
2013-03-19 16:52:18 +0000

    Add condition for essentiel and video only on the main page

--- gites/core/browser/essentiel.py
+++ gites/core/browser/essentiel.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_inner, aq_parent
 from five import grok
 from zope import interface
+from plone.app.layout.navigation.interfaces import INavigationRoot
 
 grok.templatedir('templates')
 grok.context(interface.Interface)
@@ -10,7 +12,13 @@ class EssentielViewletManager(grok.ViewletManager):
     grok.name('gites.essentiel')
 
 class EssentielViewlet(grok.Viewlet):
-    pass
+
+    def isMainPage(self):
+        obj = aq_inner(self.context)
+        if INavigationRoot.providedBy(aq_parent(obj)):
+            return True
+        else:
+            return False
 
 # register all viewlets in this viewlet manager:
 grok.viewletmanager(EssentielViewletManager)
--- gites/core/browser/footer.py
+++ gites/core/browser/footer.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_inner, aq_parent
 from five import grok
 from zope import interface
 from datetime import datetime
+from plone.app.layout.navigation.interfaces import INavigationRoot
 
 grok.templatedir('templates')
 grok.context(interface.Interface)
@@ -21,6 +23,13 @@ class VideoFooterViewlet(grok.Viewlet):
             return 'http://www.youtube.com/embed/BCJHP0T8g0k'
         else:
             return 'http://www.youtube.com/embed/qZ41Dpgqgds'
+
+    def isMainPage(self):
+        obj = aq_inner(self.context)
+        if INavigationRoot.providedBy(aq_parent(obj)):
+            return True
+        else:
+            return False
 
 class InfosFooterViewlet(grok.Viewlet):
     grok.order(20)
