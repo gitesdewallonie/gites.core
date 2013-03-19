cbd67c1798a07676e51b1db96d79000265baba04
Jean-François Roche <jfroche@affinitic.be>
2013-03-19 12:46:34 +0100

    add fetcher for package

--- gites/core/adapters/hebergementsfetcher.py
+++ gites/core/adapters/hebergementsfetcher.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from five import grok
+from sqlalchemy.orm import joinedload
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IBrowserRequest
+from affinitic.db.cache import FromCache
+from gites.db import session
+from gites.db.content import LinkHebergementMetadata, Hebergement
 from gites.core.interfaces import IHebergementsFetcher
 from gites.core.content.interfaces import IPackage
 
@@ -19,5 +23,25 @@ class BaseHebergementsFetcher(grok.MultiAdapter):
 class PackageHebergementFetcher(BaseHebergementsFetcher):
     grok.adapts(IPackage, Interface, IBrowserRequest)
 
+    @property
+    def _query(self):
+        query = session().query(Hebergement)
+        query = query.options(
+            joinedload('type', innerjoin=True),
+            joinedload('commune', innerjoin=True),
+            joinedload('epis', innerjoin=True),
+            FromCache('gdw'))
+        subquery = session().query(LinkHebergementMetadata.heb_fk)
+        for criterion in self.context.getCriteria():
+            is_true = criterion.get('value') == '1'
+            subquery = subquery.filter(LinkHebergementMetadata.metadata_fk == criterion.get('criterion'))
+            subquery = subquery.filter(LinkHebergementMetadata.link_met_value == is_true)
+        subquery = subquery.subquery()
+        query = query.filter(Hebergement.heb_pk == subquery.c.heb_fk)
+        return query
+
+    def __len__(self):
+        return self._query.count()
+
     def __call__(self):
-        import pdb;pdb.set_trace()
+        return self._query.all()
