5c1dc124aa7f0ab8e7f6b7c9acd2b8b48d981a38
Laurent Lasudry <lasudry@gmail.com>
2013-03-22 11:48:53 +0100

    Fix and improve portlets and contents migration

--- gites/core/migrations/to_0_2.py
+++ gites/core/migrations/to_0_2.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from zope.container  import contained
+from plone import api
 from Products.CMFCore.utils import getToolByName
 from gites.core.utils import clearPortlets
 
@@ -6,4 +8,44 @@ from gites.core.utils import clearPortlets
 def migrate(context):
     portal_url = getToolByName(context, 'portal_url')
     portal = portal_url.getPortalObject()
+    removePortlets(portal)
+    removeContent(portal, 'couple')
+    removeContent(portal, 'famille')
+    removeContent(portal, 'groupe')
+    removeContent(portal, 'map')
+    removeContent(portal, 'sejour-fute')
+    removeContent(portal, 'best-deal')
+    removeContent(portal, 'formation')
+    removeAllContentInsideFolder(portal, 'idee-sejour')
+    catalog = getToolByName(context, 'portal_catalog')
+    catalog.clearFindAndRebuild()
+
+
+def removePortlets(portal):
+    contained.fixing_up = True
     clearPortlets(portal)
+    associationFolder = getattr(portal, 'association')
+    clearPortlets(associationFolder)
+    zoneMembreFolder = getattr(portal, 'zone-membre')
+    clearPortlets(zoneMembreFolder)
+    ideeSejourFolder = getattr(portal, 'idee-sejour')
+    clearPortlets(ideeSejourFolder)
+    contained.fixing_up = False
+
+
+def removeAllContentInsideFolder(container, folderId):
+    # no translations here
+    folder = getattr(container, folderId)
+    for objectId in folder.objectIds():
+        if objectId:
+            content = getattr(folder, objectId)
+            api.content.delete(content)
+
+
+def removeContent(folder, objectId):
+    content = getattr(folder, objectId)
+    translations = content.getTranslations()
+    for language in translations:
+        translatedContent = translations[language][0]
+        if translatedContent:
+            api.content.delete(translatedContent)
