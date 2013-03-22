c0511b50ccd9e557b48539211f1607b82e9eacb9
Laurent Lasudry <lasudry@gmail.com>
2013-03-22 17:07:45 +0100

    Fix large photo, link and translations
    Cleanup

--- gites/core/viewlets/header.py
+++ gites/core/viewlets/header.py
@@ -17,16 +17,6 @@ class TopHeaderViewlet(grok.Viewlet):
 class MainHeaderViewlet(grok.Viewlet):
     grok.order(20)
 
-    def getTranslatedObjectUrl(self, path):
-        """
-        """
-        obj = self.context.restrictedTraverse(path)
-        translatedObject = obj.getTranslation()
-        if translatedObject:
-            url = translatedObject.absolute_url()
-        else:
-            url = obj.absolute_url()
-        return url
 
 # register all viewlets in this viewlet manager:
 grok.viewletmanager(HeaderViewletManager)
