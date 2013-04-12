497bc67a08a0ef0b8d26e02b210803cfc4e967bc
Alain Meurant <alain.meurant@affinitic.be>
2013-04-12 11:26:07 +0200

    description :: modif getanimal et getfumeur avec nom des images

--- gites/core/browser/hebergement.py
+++ gites/core/browser/hebergement.py
@@ -178,15 +178,15 @@ class HebergementView(BrowserView):
         list = self.getHebMetadatasByType('autorisations')
         for item in list:
             if item['id'] == 'heb_animal':
-                return {'id': 'dog'}
-        return {'id': 'no-dog'}
+                return {'id': 'heb_confort_animal'}
+        return {'id': 'heb_confort_no_animal'}
 
     def getFumeur(self):
         list = self.getHebMetadatasByType('autorisations')
         for item in list:
             if item['id'] == 'heb_fumeur':
-                return {'id': 'smoke'}
-        return {'id': 'no-smoke'}
+                return {'id': 'heb_confort_fumeur'}
+        return {'id': 'heb_confort_no_fumeur'}
 
     def render(self):
         return self.template()
