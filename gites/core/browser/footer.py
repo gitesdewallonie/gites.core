78408416661be2a7edb50dea3a02a4041d5b4798
Laurent Lasudry <lasudry@gmail.com>
2013-04-17 14:17:57 +0200

    Get Plone contents for footer

---
+++ gites/core/browser/footer.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+gites.core
+
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+from five import grok
+from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+
+grok.context(Interface)
+
+
+class FooterView(grok.View):
+    """
+    Return footer contents
+    """
+    grok.name('footer_view')
+    grok.require('zope2.View')
+
+    def getContent(self, contentId):
+        portal = getToolByName(self.context, 'portal_url').getPortalObject()
+        footerFolder = getattr(portal, 'footer', None)
+        if footerFolder is None:
+            return
+        content = footerFolder.restrictedTraverse(contentId, default=None)
+        if content:
+            translation = content.getTranslation()
+            return translation and translation.CookedBody() or None
+
+    def render(self):
+        pass
