e41a279a947f12234ee04c26aeec947009fcfd6b
francoisbr <francois@affinitic.be>
2013-05-23 09:27:42 +0000

    Fix signalerprobleme when no language Add required class in textbox when select other type refs Affinitic #4917

--- gites/core/browser/resources/signalerprobleme.js
+++ gites/core/browser/resources/signalerprobleme.js
@@ -2,7 +2,9 @@ function check() {
    var type = document.getElementById('typeProbleme').value;
    if (type == "autre") {
       document.getElementById('typeautre').style.display = 'inline';
+      document.getElementById('typeautre').className = 'required';
    } else if (type !== "autre" ) {
       document.getElementById('typeautre').style.display = 'none';
+      document.getElementById('typeautre').className = '';
    }
 }
