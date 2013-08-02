d81513a8c63930db5c980ef05e056c0c344e578c
Laurent Lasudry <lasudry@gmail.com>
2013-08-02 17:00:54 +0200

    Remove code that was not ready-bugproof yet (#5286)

--- gites/core/browser/resources/formchoices.js
+++ gites/core/browser/resources/formchoices.js
@@ -1,22 +1,5 @@
 jQuery(document).ready(function($) {
 
-    function toggleCheckboxes(name) {
-        checkedInputs = $("input[name='" + name + "']:checked");
-        if (checkedInputs.length == 1) {
-            checkedInputs.prop('disabled', true);
-        }
-        else {
-            checkedInputs.prop('disabled', false);
-        }
-    }
-
-    toggleCheckboxes('form.widgets.hebergementType:list');
-
-    $("input[name='form.widgets.hebergementType:list']").change(function(sender) {
-        toggleCheckboxes('form.widgets.hebergementType:list');
-    });
-
-
     // 'From' and 'To' dates should limit each other
     $("#form-widgets-fromDate").datepicker('option', 'onSelect', function(date) {
         $("#form-widgets-toDate").datepicker("option", "minDate", date);
