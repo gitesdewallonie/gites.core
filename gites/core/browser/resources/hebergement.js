344f3d4183a33edbba1fcb844093e52502e20704
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2013-09-28 11:26:47 +0200

    Fix the datepicker after an error. refs #5390

--- gites/core/browser/resources/hebergement.js
+++ gites/core/browser/resources/hebergement.js
@@ -1,30 +1,41 @@
 jQuery(document).ready(function($) {
 
+    var loadDatePicker = function() {
+      $(".datepicker-widget").datepicker({
+                                           minDate: 0,
+                                           dateFormat: "dd/mm/yy",
+                                           showOn: "both",
+                                           buttonImage: "++theme++gites.theme/images/icon_calendrier.png",
+                                           buttonImageOnly: true
+                                         });
+      $("#fromDate").datepicker('option', 'onSelect', function(date) {
+          $("#toDate").datepicker("option", "minDate", date);
+      });
+      $("#toDate").datepicker('option', 'onSelect', function(date) {
+          $("#fromDate").datepicker("option", "minDate", date);
+      });
+    };
+
     $('#contacter-proprio a').prepOverlay(
         {
             subtype: 'ajax',
             filter: '#content-for-khevine>*:not(div.configlet),dl.portalMessage.error,dl.portalMessage.info',
             cssclass: 'overlay-contact',
             formselector: 'form[name="contact-proprio"]',
+            beforepost: function(form, form_data) {
+              $(form).closest('#content-core')
+                     .load(form.attr('action') + " #contact-proprio-formulaire",
+                           form_data,
+                           function(e) {
+                loadDatePicker();
+              });
+              return false;
+            },
             config: {
-            onBeforeLoad : function (e) {
-              $(".datepicker-widget").datepicker({
-                                                   minDate: 0,
-                                                   dateFormat: "dd/mm/yy",
-                                                   showOn: "both",
-                                                   buttonImage: "++theme++gites.theme/images/icon_calendrier.png",
-                                                   buttonImageOnly: true
-                                                 });
-              $("#fromDate").datepicker('option', 'onSelect', function(date) {
-                  $("#toDate").datepicker("option", "minDate", date);
-              });
-              $("#toDate").datepicker('option', 'onSelect', function(date) {
-                  $("#fromDate").datepicker("option", "minDate", date);
-              });
-
-            return true;
+              onBeforeLoad : function (e) {
+                loadDatePicker();
+              }
             }
-        }
         }
     );
 
@@ -37,7 +48,7 @@ jQuery(document).ready(function($) {
         }
     );
 
-    $('.carousel').carousel({  
+    $('.carousel').carousel({
       interval: false
     });
 
