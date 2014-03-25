3e8cc0e26246c4128774ad5dad4139f39e9b294c
Laurent Lasudry <lasudry@gmail.com>
2014-03-25 20:47:35 +0100

    Add migration to create carousel folder and its properties This refs #5621

---
+++ gites/core/migrations/to_0_7.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+gites.core
+
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+from Products.CMFCore.utils import getToolByName
+from gites.core.utils import publishObject
+
+
+def migrate(context):
+    portal_url = getToolByName(context, 'portal_url')
+    portal = portal_url.getPortalObject()
+    portal.invokeFactory('Folder', id='carousel', title='Carousel')
+    carouselFolder = getattr(portal, 'carousel')
+    publishObject(carouselFolder)
+    carouselFolder.setLanguage('')
+    carouselFolder.setConstrainTypesMode(1)
+    carouselFolder.setLocallyAllowedTypes(['Image', 'Link'])
+    carouselFolder.setImmediatelyAddableTypes(['Image', 'Link'])
+    carouselFolder.reindexObject()
