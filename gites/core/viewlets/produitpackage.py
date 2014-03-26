4da30317f9dc2608d43ccce3ad16b1125d6cf33d
Laurent Lasudry <lasudry@gmail.com>
2014-03-26 11:37:10 +0100

    Add static image carousel replacement mechanism This refs #5621

--- gites/core/viewlets/produitpackage.py
+++ gites/core/viewlets/produitpackage.py
@@ -2,6 +2,7 @@
 import random
 from five import grok
 from zope import interface
+from plone.memoize.instance import memoize
 from Products.CMFCore.utils import getToolByName
 
 grok.templatedir('templates')
@@ -13,6 +14,44 @@ class ProduitPackageViewletManager(grok.ViewletManager):
 
 
 class ProduitPackageViewlet(grok.Viewlet):
+
+    @memoize
+    def getVisualImage(self):
+        utool = getToolByName(self.context, 'portal_url')
+        portal = utool.getPortalObject()
+        carouselFolder = getattr(portal, 'carousel', None)
+        if not carouselFolder:
+            return []
+        currentLanguage = self.request.get('LANGUAGE', 'en')
+        cat = getToolByName(self.context, 'portal_catalog')
+        path = '/'.join(carouselFolder.getPhysicalPath())
+        images = cat.searchResults(portal_type=['Image'],
+                                   path={'query': path})
+        results = []
+        for image in images:
+            imageObj = None
+            imageLang = ''
+            try:
+                imageLang = image.getLanguage()
+            except AttributeError:
+                imageObj = image.getObject()
+                imageLang = imageObj.getLanguage()
+            if imageLang != '' and imageLang != currentLanguage:
+                continue
+            if imageObj is None:
+                imageObj = image.getObject()
+            relatedItems = imageObj.getRelatedItems()
+            for relatedItem in relatedItems:
+                if relatedItem.portal_type != 'Link':
+                    continue
+                linkLang = relatedItem.getLanguage()
+                if linkLang == '' or linkLang == currentLanguage:
+                    visualImage = {'imagesrc': image.getURL(),
+                                   'linkhref': relatedItem.remoteUrl}
+                    results.append(visualImage)
+        if results:
+            random.shuffle(results)
+            return results[0]
 
     def getCarouselPackages(self):
         cat = getToolByName(self.context, 'portal_catalog')
